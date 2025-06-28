ffb34b9cd726536c04ea5da3c8c594b7f9d99f31
Antony1012ish <karedanantony@gmail.com>
2025-06-28 18:20:53 +0530

    Fix: test deployment with minimal FastAPI app

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from database import engine
-import models
-from routers import students, classes
-
-models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "FastAPI app deployed successfully"}
-
-app.include_router(students.router, prefix="/students", tags=["Students"])
-app.include_router(classes.router, prefix="/classes", tags=["Classes"])
+def read_root():
+    return {"message": "FastAPI deployed successfully!"}
